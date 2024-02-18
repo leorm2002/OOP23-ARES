7d1b53a9505f38597fcffd54976e2fd999407996
Uhalid Abou El kheir <uhalid.abouelkheir@gmail.com>
2024-02-18 18:33:42 +0100

    javadoc Ticker

--- src/main/java/it/unibo/ares/core/controller/Ticker.java
+++ src/main/java/it/unibo/ares/core/controller/Ticker.java
@@ -1,7 +1,22 @@
 package it.unibo.ares.core.controller;
 
+/**
+ * The Ticker interface provides a mechanism for objects to implement a
+ * start-stop functionality.
+ * Implementations of this interface are expected to define what actions should
+ * be taken when the ticker is started or stopped.
+ */
 public interface Ticker {
+    /**
+     * Starts the ticker, initiating any scheduled or repetitive tasks that should
+     * be run.
+     */
     void start();
 
+    /**
+     * Stops the ticker, halting any ongoing tasks and preventing new ones from
+     * starting.
+     * This method should also handle the necessary cleanup.
+     */
     void stop();
 }
--- src/main/java/it/unibo/ares/core/controller/TickerImpl.java
+++ src/main/java/it/unibo/ares/core/controller/TickerImpl.java
@@ -4,12 +4,25 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * This class implements the Ticker interface and provides a mechanism for
+ * executing a task at a fixed rate.
+ * It uses a ScheduledExecutorService to schedule the task.
+ */
 public class TickerImpl implements Ticker {
     private final ScheduledExecutorService scheduler;
     private final long initialDelay;
     private final long period;
     private final Runnable tickTask;
 
+    /**
+     * Constructs a new TickerImpl.
+     *
+     * @param tickTask     the task to be executed at a fixed rate
+     * @param initialDelay the delay (in seconds) before the task is first executed
+     * @param period       the period (in seconds) between successive executions of
+     *                     the task
+     */
     public TickerImpl(final Runnable tickTask, final long initialDelay, final long period) {
         this.scheduler = Executors.newSingleThreadScheduledExecutor();
         this.tickTask = tickTask;
@@ -17,11 +30,19 @@ public class TickerImpl implements Ticker {
         this.period = period;
     }
 
+    /**
+     * Starts the execution of the task at a fixed rate.
+     */
     @Override
     public void start() {
         scheduler.scheduleAtFixedRate(tickTask, initialDelay, period, TimeUnit.SECONDS);
     }
 
+    /**
+     * Stops the execution of the task. If tasks are in progress, it waits for 60
+     * seconds for them to finish.
+     * If tasks do not finish in 60 seconds, it attempts to stop them forcefully.
+     */
     @Override
     public void stop() {
         scheduler.shutdown();
