45afc62a6b2e4a29e3ea4a3ff64d19cd1de402fa
loredv17 <divaranolorenzo@gmail.com>
2024-02-24 17:03:12 +0100

    fix to direction in fire spread model

--- src/main/java/it/unibo/ares/core/model/FireSpreadModelFactory.java
+++ src/main/java/it/unibo/ares/core/model/FireSpreadModelFactory.java
@@ -100,6 +100,5 @@ public class FireSpreadModelFactory implements ModelFactory {
                                         }
                                 })
                                 .build();
-
         }
 }
